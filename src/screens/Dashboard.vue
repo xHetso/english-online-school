afceef5a92481419c0e1f96e367a7ce3b42373b9
Hetso <datacener23491248@gmail.com>
2024-05-31 04:57:53 +0500

    пофиксил

--- src/screens/Dashboard.vue
+++ src/screens/Dashboard.vue
@@ -30,77 +30,37 @@ function goToCourses() {
 </script>
 
 <template>
-  <div class="flex flex-col justify-center items-center p-10 bg-gradient-to-r from-indigo-700 via-purple-700 to-pink-700 rounded-3xl">
-    <h1 class="text-3xl mb-2">Курстан не аласыз?</h1>
-    <div class="max-w-4xl mx-auto p-10">
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
-        <div @click="goToCourses" class="card bg-gradient-to-tr from-gray-700 to-gray-800 rounded-lg shadow-lg p-6">
-          <h3 class="text-xl text-indigo-500 font-semibold mb-4">Терең білім</h3>
-          <p>
-            Біз веб-технологиялар бойынша курстардың кең спектрін ұсынамыз, оған JavaScript, React JS, Nest JS және басқалар кіреді.
-          </p>
-        </div>
-        <div @click="goToCourses" class="card bg-gradient-to-tr from-gray-700 to-gray-800 rounded-lg shadow-lg p-6">
-          <h3 class="text-xl text-indigo-500 font-semibold mb-4">Эксклюзивті сабақтар</h3>
-          <p>
-            Біздің тегін білімдерімізге қосымша ретінде, сізге ерекше веб-сайт жасауға көмектесеміз.
-          </p>
-        </div>
-        <div @click="goToCourses" class="card bg-gradient-to-tr from-gray-700 to-gray-800 rounded-lg shadow-lg p-6">
-          <h3 class="text-xl text-indigo-500 font-semibold mb-4">Оңай оқу</h3>
-          <p>
-            Біздің материалдар қарапайым және қызықты түрде берілген, бұл оқуды жеңіл және тартымды етеді.
-          </p>
-        </div>
-        <div @click="goToCourses" class="card bg-gradient-to-tr from-gray-700 to-gray-800 rounded-lg shadow-lg p-6">
-          <h3 class="text-xl text-indigo-500 font-semibold mb-4">Бастапқыдан кәсіби деңгейге дейін</h3>
-          <p>
-            Көптеген курстардан өтіп, белсенділік танытқаннан кейін, біздің командамызға қосылу мүмкіндігіңіз бар.
-          </p>
-        </div>
+<div class="flex flex-col justify-center items-center p-10 bg-gradient-to-r from-indigo-700 via-purple-700 to-pink-700 rounded-3xl">
+  <h1 class="text-3xl mb-2">Ағылшын тілін оқу арқылы тілді меңгеру!</h1>
+  <div class="max-w-4xl mx-auto p-10">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+      <div @click="goToCourses" class="card bg-gradient-to-tr from-gray-700 to-gray-800 rounded-lg shadow-lg p-6">
+        <h3 class="text-xl text-indigo-500 font-semibold mb-4">Түрлі мәтіндер мен әдеби шығармаларды қызықты оқу арқылы ағылшын тілі әлеміне еніңіз.</h3>
+        <p>
+          Ағылшын тілінің өзіне арналған қосымша тексттер мен әдебиеттің өзінен көз жеткізіңіз.
+        </p>
+      </div>
+      <div @click="goToCourses" class="card bg-gradient-to-tr from-gray-700 to-gray-800 rounded-lg shadow-lg p-6">
+        <h3 class="text-xl text-indigo-500 font-semibold mb-4">Материалдардың бейнелері</h3>
+        <p>
+          Біздің сайтымызда әр оқушының қызығушылығына сәйкес әртүрлі тақырыптардағы мәтіндердің кең ауқымы ұсынылады.
+        </p>
+      </div>
+      <div @click="goToCourses" class="card bg-gradient-to-tr from-gray-700 to-gray-800 rounded-lg shadow-lg p-6">
+        <h3 class="text-xl text-indigo-500 font-semibold mb-4">Сіздің деңгейіңізге бағытталған оқу процесі</h3>
+        <p>
+          Ағылшын тілінің деңгейіне қарамастан, біздің сайт оқу және түсіну дағдыларыңызды дамытуға көмектеседі.
+        </p>
+      </div>
+      <div @click="goToCourses" class="card bg-gradient-to-tr from-gray-700 to-gray-800 rounded-lg shadow-lg p-6">
+        <h3 class="text-xl text-indigo-500 font-semibold mb-4">Жаңа әлем ашу үшін оқу</h3>
+        <p>
+          Ағылшын тілінде мәтіндерді оқығанмен қоса, жаңа дүниенің қаһарына жүгірген боласыз. 
+        </p>
       </div>
     </div>
   </div>
-
-  <div class="py-10 bg-gray-800 rounded-3xl mt-10 p-10">
-    <h2 class="text-3xl font-bold text-center mb-10 text-white">Біздің оқытушылар</h2>
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-      <div 
-        v-for="author in authors" 
-        :key="author._id" 
-        class="transform transition duration-300 hover:-translate-y-2 cursor-pointer"
-        @click="goToCourses"
-      >
-        <img 
-          :src="'https://backend-english-school.onrender.com'+author.photo" 
-          :alt="author.name" 
-          class="w-full aspect-square object-cover rounded-t-3xl"
-        >
-        <div class="p-4 bg-purple-800 rounded-b-3xl shadow-lg">
-          <h3 class="text-xl font-semibold text-white">{{ author.name }}</h3>
-          <p class="text-white">Сабақтар саны: {{ author.countLessons }}</p>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div class="py-10 bg-gradient-to-r from-indigo-700 via-purple-700 to-pink-700 rounded-3xl mt-10 p-10">
-    <h2 class="text-3xl font-bold text-center mb-10 text-white">Біздің студенттер не дейді</h2>
-    <Swiper
-      :slidesPerView="3"
-      :spaceBetween="30"
-      :pagination="{ clickable: true }"
-      class="mySwiper"
-    >
-      <SwiperSlide v-for="comment in comments" :key="comment.id">
-        <div class="flex flex-col items-center">
-          <img :src="comment.photo" alt="Студенттің фотосы" class="w-[200px] h-[200px] rounded-full object-cover">
-          <p class="mt-4 text-2xl text-white font-semibold">{{ comment.name }}</p>
-          <p class="text-white text-xl text-center">{{ comment.comment }}</p>
-        </div>
-      </SwiperSlide>
-    </Swiper>
-  </div>
+</div>
 
 </template>
 
